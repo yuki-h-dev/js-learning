a09418adeb458d3fc104c6d86063b8b06f3dcc94
Yuki H <soccer.26.promise@gmail.com>
2025-12-13 16:20:12 +1100

    feat: add todo item

--- todo-app-js/main.js
+++ todo-app-js/main.js
@@ -1,1 +1,31 @@
-import "./style.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.classList.add("list-row");
+
+  const p = document.createElement("P");
+  p.classList.add("todo-item");
+  p.innerText = inputText;
+
+  const completeBtn = document.createElement("button");
+  completeBtn.innerText = "完了";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "削除";
+
+  div.appendChild(p);
+  div.appendChild(completeBtn);
+  div.appendChild(deleteBtn);
+  li.appendChild(div);
+
+  const incompleteList = document.getElementById("incomplete-list");
+  incompleteList.appendChild(li);
+};
+
+const addBtn = document.getElementById("add-btn");
+
+addBtn.addEventListener("click", onClickAdd);
